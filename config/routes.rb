e8f7d775e6373451edd0a2ef9054004ac972567d
Josh Lavallee <jlavallee76@academic.rrc.ca>
2021-02-11 12:06:53 -0600

    Configured Product View and Route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  root to: "home#index"
+  resources :products, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
